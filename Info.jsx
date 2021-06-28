556095a436e6281199912b22a00edc7518e5cf82
Hanumanthu Madhuri <79242504+Hanumanthu-Madhuri9@users.noreply.github.com>
2021-06-28 11:43:45 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>React.js</h1>
+      <p>
+        React.js is an open-source frontend framework that is based on
+        JavaScript
+      </p>
+    </div>
+  );
+}
+
+export default Info;
